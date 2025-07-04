7d58e1fdecf51464ca5a52a2f1a9213c0c15570a
nagai <nagai@ubuntu20.04.myguest.virtualbox.org>
2025-07-05 01:41:31 +0900

    add linetrace_code

---
+++ p_ctrl/p_ctrl.ino
@@ -0,0 +1,118 @@
+// --- ピン定義 ---
+const int sensorPins[8] = {26, 25, 33, 32, 35, 34, 39, 36};
+const int sensorWeights[8] = {-4, -3, -2, -1, 1, 2, 3, 4};
+
+// たぶん左
+const int ENA = 17;
+const int IN1 = 16;
+const int IN2 = 4;
+
+// たぶん右
+const int ENB = 15;
+const int IN3 = 0;
+const int IN4 = 2;
+
+const int pwmChannelA = 0;
+const int pwmChannelB = 1;
+const int pwmFreq = 5000;
+const int pwmResolution = 8;
+
+// --- 制御パラメータ ---
+const int baseSpeed = 100;
+const float Kp = 130.0;
+
+volatile float position = 0.0;
+volatile int total = 0;
+volatile int lastKnownDirection = 0;
+
+// --- タスクハンドル ---
+TaskHandle_t sensorTaskHandle;
+TaskHandle_t motorTaskHandle;
+
+void setup() {
+  Serial.begin(115200);
+
+  // PWM設定
+  ledcSetup(pwmChannelA, pwmFreq, pwmResolution);
+  ledcAttachPin(ENA, pwmChannelA);
+  ledcSetup(pwmChannelB, pwmFreq, pwmResolution);
+  ledcAttachPin(ENB, pwmChannelB);
+
+  // モーター方向ピン
+  pinMode(IN1, OUTPUT); pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT); pinMode(IN4, OUTPUT);
+
+  digitalWrite(IN1, HIGH); digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH); digitalWrite(IN4, LOW);
+
+  // タスク作成
+  xTaskCreatePinnedToCore(sensorTask, "SensorTask", 2048, NULL, 1, &sensorTaskHandle, 0); // Core 0
+  xTaskCreatePinnedToCore(motorTask, "MotorTask", 2048, NULL, 1, &motorTaskHandle, 1);     // Core 1
+}
+
+void sensorTask(void *pvParameters) {
+  while (true) {
+    int sensorValues[8];
+    int weightedSum = 0;
+    int sum = 0;
+
+    for (int i = 0; i < 8; i++) {
+      sensorValues[i] = analogRead(sensorPins[i]);
+      weightedSum += sensorValues[i] * sensorWeights[i];
+      sum += sensorValues[i];
+    }
+
+    Serial.printf("%d %d %d %d %d %d %d %d \n",
+                  sensorValues[0], sensorValues[1], sensorValues[2], sensorValues[3],
+                  sensorValues[4], sensorValues[5], sensorValues[6], sensorValues[7]);
+
+    if (sum != 0) {
+      position = (float)weightedSum / sum;
+    } else {
+      position = 0.0;
+    }
+
+    total = sum;
+
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+  }
+}
+
+void motorTask(void *pvParameters) {
+  while (true) {
+    if (total < 1500) {
+      if (lastKnownDirection > 0) {
+        ledcWrite(ENA, 0);
+        ledcWrite(ENB, 0);
+      } else if (lastKnownDirection < 0) {
+        ledcWrite(ENA, 0);
+        ledcWrite(ENB, 255);
+      } else {
+        ledcWrite(ENA, 0);
+        ledcWrite(ENB, 0);
+      }
+      vTaskDelay(100 / portTICK_PERIOD_MS);
+      continue;
+    }
+
+    float turn = Kp * position;
+
+    int leftSpeed = constrain(baseSpeed + turn, 0, 255);
+    int rightSpeed = constrain(baseSpeed - turn, 0, 255);
+
+    ledcWrite(ENA, leftSpeed);
+    ledcWrite(ENB, rightSpeed);
+
+    if (position > 0.5) {
+      lastKnownDirection = 1;
+    } else if (position < -0.5) {
+      lastKnownDirection = -1;
+    }
+
+    vTaskDelay(20 / portTICK_PERIOD_MS);
+  }
+}
+
+void loop() {
+  delay(1000);
+}
